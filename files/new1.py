1f6f7a014737f79e6bc409a905ef86f8488ad467
Sidra Sonia Aziz <2019ce8@student.uet.edu.pk>
2024-02-09 19:18:48 +0500

    Update new1.py

--- files/new1.py
+++ files/new1.py
@@ -1,6 +1,6 @@
 from pyannote.audio import Pipeline
 pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization@2.1",
-                                    use_auth_token="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")
+                                    use_auth_token="#paste token here")
 
 audio = "uploads/JaspreetSingh_JayShetty_2.wav"
 
